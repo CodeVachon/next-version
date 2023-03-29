62e414707289fe180e6078668f405f19b0c16617
Christopher Vachon <cvachon@prometrix.ca>
2023-03-29 13:49:38 -0400

    Update to Handle PNPM Workspaces

--- src/packageJsonUtls.ts
+++ src/packageJsonUtls.ts
@@ -1,14 +1,16 @@
 import { readFileSync, writeFileSync } from "fs";
 import { resolve } from "path";
 
+const fixCWD = (cwd: string) => String(cwd).replace(new RegExp("package\\.json$", "i"), "");
+
 export const readPackageJson = (cwd: string) => {
-    const packageJsonContent = readFileSync(resolve(cwd, "package.json"), "utf8");
+    const packageJsonContent = readFileSync(resolve(fixCWD(cwd), "package.json"), "utf8");
 
     return packageJsonContent;
 };
 
 export const writePackageJson = (cwd: string, content: string) => {
-    writeFileSync(resolve(cwd, "package.json"), content, {
+    writeFileSync(resolve(fixCWD(cwd), "package.json"), content, {
         flag: "w"
     });
 };
